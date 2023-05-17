f18d2e4448054a0f1b105d06088198481ffdb7a6
prameshbajra <pe.messh@gmail.com>
2023-05-17 21:50:33 +0200

    Not working - Zip size too big.

---
+++ src/app.py
@@ -0,0 +1,25 @@
+import json
+import time
+import boto3
+import openai
+import whisper
+
+start = time.time()
+model = whisper.load_model("base")
+end = time.time()
+print("Whisper Model load time: ", end - start)
+
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager', region_name='us-east-1')
+
+openai.api_key = client.get_secret_value(
+    SecretId='OPENAI_API_KEY')['SecretString']
+
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "event": event,
+        }),
+    }
